81c0dd5a79e5a949afadf0631f29102784d23bfb
João Nery <ojoaonery@gmail.com>
2024-10-14 08:53:56 -0300

    feat: add Student model

---
+++ Students/Students.Api/Models/Student.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Students.Api.Models;
+
+public class Student
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public string Ra { get; private set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O email deve ser válido.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "O CPF é obrigatório.")]
+    [StringLength(11, ErrorMessage = "O CPF deve ter 11 caracteres.")]
+    public string Cpf { get; private set; }
+
+    public Student (string ra, string nome, string email, string cpf)
+    {
+        Ra = ra;
+        Nome = nome;
+        Email = email;
+        Cpf = cpf;
+    }   
+}
